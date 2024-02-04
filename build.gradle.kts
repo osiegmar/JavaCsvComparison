bae951b4517e9b9a99c255b386a09d036cd4c83b
Oliver Siegmar <oliver@siegmar.de>
2024-02-04 10:54:27 +0100

    gradle and java update

---
+++ build.gradle.kts
@@ -0,0 +1,31 @@
+plugins {
+    application
+    checkstyle
+}
+
+java {
+    toolchain {
+        languageVersion = JavaLanguageVersion.of(21)
+    }
+}
+
+application {
+    mainClass = "comparison.Comparison"
+}
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    implementation("com.fasterxml.jackson.dataformat:jackson-dataformat-csv:2.16.1")
+    implementation("com.github.skjolber.sesseltjonna-csv:parser:1.0.25")
+    implementation("com.opencsv:opencsv:5.9")
+    implementation("com.univocity:univocity-parsers:2.9.1")
+    implementation("de.siegmar:fastcsv:3.0.0")
+    implementation("net.sf.supercsv:super-csv:2.4.0")
+    implementation("net.sourceforge.javacsv:javacsv:2.0")
+    implementation("net.steppschuh.markdowngenerator:markdowngenerator:1.3.1.1")
+    implementation("org.apache.commons:commons-csv:1.10.0")
+    implementation("org.simpleflatmapper:sfm-csv:8.2.3")
+}
---
+++ settings.gradle.kts
@@ -0,0 +1,5 @@
+rootProject.name = "JavaCsvComparison"
+
+plugins {
+    id("org.gradle.toolchains.foojay-resolver-convention") version "0.8.0"
+}

a5729fff1be87563d7bfdce86342f2d0f4a9a55e
Philippe Charles <charphi@users.noreply.github.com>
2024-11-04 06:17:17 +0100

    Improve the passed checks of picocsv (#4)

--- impl/picocsv/src/test/java/picocsv/PicoCsvTest.java
+++ impl/picocsv/src/test/java/picocsv/PicoCsvTest.java
@@ -3,10 +3,12 @@ package picocsv;
 import java.io.IOException;
 import java.io.StringReader;
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 
 import comparison.AbstractTest;
 import nbbrd.picocsv.Csv;
+import specreader.spec.TestSpecCommentMode;
 import specreader.spec.TestSpecSettings;
 
 public class PicoCsvTest extends AbstractTest {
@@ -19,17 +21,26 @@ public class PicoCsvTest extends AbstractTest {
         throws IOException {
         final List<List<String>> ret = new ArrayList<>();
 
-        final Csv.Reader csv = configure(input);
+        final Csv.Reader csv = configure(settings, input);
         while (csv.readLine()) {
             if (csv.isComment() && settings.commentMode() == specreader.spec.TestSpecCommentMode.SKIP) {
                 // emulating the behavior of other libraries
                 continue;
             }
 
-            final List<String> row = new ArrayList<>();
-            while (csv.readField()) {
-                row.add(csv.toString());
+            if (!csv.readField()) {
+                // emulating the behavior of other libraries
+                if (settings.skipEmptyLines()) {
+                    continue;
+                }
+                ret.add(Collections.emptyList());
+                continue;
             }
+
+            final List<String> row = new ArrayList<>();
+            do {
+                row.add(csv.toString());
+            } while (csv.readField());
 
             ret.add(row);
         }
@@ -37,24 +48,17 @@ public class PicoCsvTest extends AbstractTest {
         return ret;
     }
 
-    private static Csv.Reader configure(final String input) throws IOException {
+    private static Csv.Reader configure(final TestSpecSettings settings, final String input) throws IOException {
         final Csv.Format format = Csv.Format.RFC4180.toBuilder()
-            .acceptMissingField(false)
+            .acceptMissingField(settings.skipEmptyLines())
+            .comment(settings.commentMode() == TestSpecCommentMode.NONE ? '\0' : '#')
             .build();
-
-        // picocsv does not support disabling comments (which are not part of RFC 4180)
 
         final Csv.ReaderOptions options = Csv.ReaderOptions.builder()
             .lenientSeparator(true)
             .build();
 
         return Csv.Reader.of(format, options, new StringReader(input));
-    }
-
-    @Override
-    protected boolean isSettingsSupported(final TestSpecSettings settings) {
-        // picocsv does not support skipping empty lines
-        return !settings.skipEmptyLines();
     }
 
 }
